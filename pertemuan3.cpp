372ebf4f8782d2272f3273551dcca1f92852713f
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-03-13 11:21:00 +0700

    tambah kondisi

---
+++ pertemuan3.cpp
@@ -0,0 +1,28 @@
+ #include <iostream>
+using namespace std;
+ double rerata(double a, double b){
+      return (a+b)/2;
+ }
+  
+  string status(double c){
+     if (c >= 60)
+       return "lulus";
+    else 
+       return "gagal";
+  }
+
+  int main (){
+    double nilM,nilB;
+    //double rata;
+    //string st;
+    cout << "Masukan niali Matematika = ";
+    cin  >>  nilM;
+    cout << "Masukan nilai Bahasa Indonesia = ";
+    cin  >>  nilB;
+    //rata = rerata(nilM,nilB);
+    //st = status(rata);
+    // cout << "Status kelulusan = " << st;
+    // cout << "Status kelulusan = " << status(rata);
+    cout << "status kelulusan = " << status(rerata(nilM,nilB));
+    return 0;
+  }
